dad0c2f7b060e67b89eb8d65f65c4c5a277d7f0d
Hemanth-Sekhar <khsekhar9@gmail.com>
2025-11-22 19:03:26 +0530

    Enhance comments in data_types_def_values.sv
    Added detailed comments about the file's purpose, including default values for various SystemVerilog data types.

--- Data Types/01_data_types_def_values.sv
+++ Data Types/01_data_types_def_values.sv
@@ -1,3 +1,27 @@
+// -----------------------------------------------------------------------------
+// File Name   : 01_data_types_def_values.sv
+// Date        : 22-11-2025
+// Description : Displays the default values of basic SystemVerilog data types
+//               including reg, wire, integer, real, time, realtime, bit, byte,
+//               logic, shortint, int, longint, and shortreal. This serves as a
+//               reference for understanding initialization behavior of various
+//               SV datatypes.
+//
+// Copyright (c) 2025 Hemanth Sekhar
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this HDL source file to use, copy, modify, merge, publish, or distribute
+// it for educational or non-commercial purposes, provided that this notice is
+// included in all copies or substantial portions of the file.
+//
+// THIS HDL SOURCE FILE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// FUNCTIONAL CORRECTNESS, PERFORMANCE, OR FITNESS FOR A PARTICULAR PURPOSE.
+//
+// Author      : Hemanth Sekhar
+// Email       : khsekhar9@gmail.com
+// -----------------------------------------------------------------------------
+
 module data_types_def_val_hs;
 
   reg        reg_type;
